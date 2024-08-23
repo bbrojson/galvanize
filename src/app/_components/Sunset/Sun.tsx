277a90914eeefc8765d206297e04eab36242a38d
bbrojson <blazej.brojson@gmail.com>
2024-08-24 00:30:34 +0200

    We need a new fuse.

--- src/app/_components/Sunset/Sun.tsx
+++ src/app/_components/Sunset/Sun.tsx
@@ -8,28 +8,30 @@ export function Sun() {
   }
 
   return (
-    <div id="sun">
-      <div id="nightbg"></div>
-      <div className="zzz1"></div>
-      <div className="zzz2"></div>
-      <div className="zzz3"></div>
-      <button id="button" onClick={handleSunset}>
-        Click me
-      </button>
-      <div className="planet">
-        <div className="face">
-          <div className="eye">
-            <div className="eye-in"></div>
-          </div>
-          <div className="mouth"></div>
-          <div className="eye">
-            <div className="eye-in"></div>
+    <div className="sunwrapper">
+      <div id="sun">
+        <div id="nightbg"></div>
+        <div className="zzz1"></div>
+        <div className="zzz2"></div>
+        <div className="zzz3"></div>
+        <button id="button" onClick={handleSunset}>
+          Click me
+        </button>
+        <div className="planet">
+          <div className="face">
+            <div className="eye">
+              <div className="eye-in"></div>
+            </div>
+            <div className="mouth"></div>
+            <div className="eye">
+              <div className="eye-in"></div>
+            </div>
           </div>
         </div>
+        <div className="star pos-star1"></div>
+        <div className="star pos-star2"></div>
+        <div className="star pos-star3"></div>
       </div>
-      <div className="star pos-star1"></div>
-      <div className="star pos-star2"></div>
-      <div className="star pos-star3"></div>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Sun } from "./_components/Sunset/Sun";
 export default async function Home() {
   return (
     <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <main className="flex h-screen flex-col items-center justify-center overflow-hidden">
         <Sun />
       </main>
     </HydrateClient>
