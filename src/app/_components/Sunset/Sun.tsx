b815c7efde3996ebe300ce735aaf7a12bf8d95df
bbrojson <blazej.brojson@gmail.com>
2024-08-24 00:05:37 +0200

    Up, Up, Down, Down, Left, Right, Left, Right, B, A.

---
+++ src/app/_components/Sunset/Sun.tsx
@@ -0,0 +1,35 @@
+"use client";
+import React from "react";
+import "./sun.css";
+
+export function Sun() {
+  function handleSunset() {
+    window.document.body.classList.toggle("day");
+  }
+
+  return (
+    <div id="sun">
+      <div id="nightbg"></div>
+      <div className="zzz1"></div>
+      <div className="zzz2"></div>
+      <div className="zzz3"></div>
+      <button id="button" onClick={handleSunset}>
+        Click me
+      </button>
+      <div className="planet">
+        <div className="face">
+          <div className="eye">
+            <div className="eye-in"></div>
+          </div>
+          <div className="mouth"></div>
+          <div className="eye">
+            <div className="eye-in"></div>
+          </div>
+        </div>
+      </div>
+      <div className="star pos-star1"></div>
+      <div className="star pos-star2"></div>
+      <div className="star pos-star3"></div>
+    </div>
+  );
+}
