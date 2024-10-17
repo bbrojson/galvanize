876c0e1d557403d100cb5d644f1845fc8579d282
bbrojson <blazej.brojson@gmail.com>
2024-10-17 18:35:56 +0200

    The moon gathers stars for the sun, but the sun only collects planets.

--- src/app/_components/shared/AppLayout.tsx
+++ src/app/_components/shared/AppLayout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <main className="flex h-screen max-h-full flex-col items-center justify-center overflow-hidden bg-[#151843] text-[#ebf3fe]">
+    <main className="flex max-h-full flex-col items-center justify-center overflow-hidden bg-[#151843] text-[#ebf3fe]">
       {children}
     </main>
   );
